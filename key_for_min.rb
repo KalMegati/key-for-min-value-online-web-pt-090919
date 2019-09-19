c9a31d39774b4f3d8ac6e5cc07fd7f865c94bdb9
Srikalyan Megati <kalmegati@gmail.com>
2019-09-19 02:59:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each { |k, v|
-    if min_key = nil
-      min_key = k
-    elsif name_hash[min_key] > v
+    if min_key
+      min_key = k if name_hash[min_key] > v
+    else 
       min_key = k
     end
   }
